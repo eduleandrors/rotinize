6866df8593071ff7deeb7efad0574cfdb56abb0b
Eduardo Leandro <edule4ndr0@gmail.com>
2025-07-12 01:12:49 -0300

    running build

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require ('sequelize');
 
-const sequelize = new Sequelize('rotinize', 'postgres', '220683',{
-    host: 'localhost',
-    dialect: 'postgres',
+const sequelize = new Sequelize('u492019782_rotinize', 'u492019782_root', 'ccz]%nLJaYGh4nHD',{
+    host: '193.203.175.197',
+    dialect: 'mysql',
+    port: 3306,
+    logging: false,
 });
+
+sequelize.authenticate()
+  .then(() => console.log('Conexão com MySQL bem-sucedida!'))
+  .catch(err => console.error('Erro na conexão:', err));
 
 module.exports = sequelize;
